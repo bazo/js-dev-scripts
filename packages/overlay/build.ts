ef16d737a0f6bc55fe69a8de3fa48900794070d4
Martin Bažík <martin@bazo.sk>
2021-03-05 12:45:55 +0100

    update overlay build script

--- packages/overlay/build.ts
+++ packages/overlay/build.ts
@@ -1,20 +1,30 @@
-import execa from "execa";
+import * as esbuild from "esbuild";
 import { resolve } from "path";
 import { cleanBuildFolder } from "../../build-functions";
 
 const buildFolder = resolve("./dist");
 
+const esbuildOptions: esbuild.BuildOptions = {
+	outfile: "dist/index.js",
+	define: {
+		"process.env.NODE_ENV": '"production"',
+	},
+	color: true,
+	write: true,
+	minify: true,
+	target: "es6",
+	logLevel: "error",
+	entryPoints: ["src/dev.tsx"],
+	pure: [],
+	tsconfig: "./tsconfig.json",
+	bundle: true,
+	format: "esm",
+};
+
 async function build(): Promise<void> {
 	cleanBuildFolder(buildFolder);
 
-	await execa.command(
-		`esbuild src/dev.tsx --outfile=dist/index.js --bundle --minify --sourcemap=inline --define:process.env.NODE_ENV="production" --format=esm --tsconfig=./tsconfig.json`,
-		{
-			extendEnv: true,
-			windowsHide: false,
-			cwd: process.cwd(),
-		}
-	);
+	esbuild.build(esbuildOptions);
 }
 
 build();
