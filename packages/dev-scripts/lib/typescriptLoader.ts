4b3842e92bc6119630407b5b5646567122206fed
Martin Bažík <martin@bazo.sk>
2021-03-15 16:51:48 +0100

    fix module paths

--- packages/dev-scripts/lib/typescriptLoader.ts
+++ packages/dev-scripts/lib/typescriptLoader.ts
@@ -4,6 +4,9 @@ import * as vm from "vm";
 export function typeScriptLoaderSync(filepath: string, content: string): Record<string, any> | null {
 	const code = transpileModule(content, { compilerOptions: { module: ModuleKind.CommonJS } });
 
+	if (module.paths === undefined) {
+		module.paths = [];
+	}
 	module.paths = module.paths.concat([`${process.cwd()}/node_modules`]);
 
 	const script = new vm.Script(code.outputText);
